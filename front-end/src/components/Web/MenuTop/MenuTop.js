b0a2297fb9a658a26a98d18d4609567eb8b68139
3rchuss <3rchuss@gmail.com>
2021-03-02 20:36:46 +0100

    empezando frontend

--- front-end/src/components/Web/MenuTop/MenuTop.js
+++ front-end/src/components/Web/MenuTop/MenuTop.js
@@ -9,28 +9,43 @@ import './MenuTop.scss';
 
 
 export default function MenuTop() { 
-    const { menuData, setMenuData } = useState([]);
+  const [menuData, setMenuData] = useState([]);
 
-    useEffect(() => {
-        getMenuApi().then(response => {
-            response.menus.forEach(menu => {
-                console.log(menu);
-            });
-        });
-
-    }, [])
+  useEffect(() => {
+    getMenuApi().then(response => {
+      const arrayItems = [];
+      response.menus.forEach(menu => {
+        menu.title.toLowerCase().indexOf("top") > -1 && arrayItems.push(...menu.items);
+      });
+      setMenuData(arrayItems);
+    });
+  }, []);
 
     return (
       <Menu className="menu-top-web" mode="horizontal">
-            <Menu.Item className="menu-top-web__logo">
-                <Link to={"/"}>
-                    <img src={logo} alt="appmaku"></img>
-                </Link>
+        <Menu.Item className="menu-top-web__logo">
+          <Link to={"/"}>
+            <img src={logo} alt="appmaku"></img>
+          </Link>
         </Menu.Item>
-        <Menu.Item className="menu-top-web__item">
-          <Link to={"/contact"}>Contacto</Link>
-        </Menu.Item>
-        <div className="menu-top-web__social">social...</div>
+        {
+          menuData.map((item) => {
+            const external = item.url.indexOf("http") > -1 ? true : false;
+            if (external) {
+              return (
+                <Menu.Item key={item._id} className="menu-top-web__item">
+                  <a href={item.url}>{item.title}</a>
+                </Menu.Item>
+              )
+            } else {
+              return (
+                <Menu.Item key={item._id} className="menu-top-web__item">
+                  <Link to={item.url}>{item.title}</Link>
+                </Menu.Item>
+              )
+            }
+          }
+        )}
       </Menu>
     );
  }
